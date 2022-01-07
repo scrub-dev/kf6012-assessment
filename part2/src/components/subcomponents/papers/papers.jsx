3f7a34fe2c680fc2c2e40374797d965804b6e7ec
Scrub <scott-donaldson@outlook.com>
2022-01-07 02:09:36 +0000

    componentDidMount() Pages

--- part2/src/components/subcomponents/papers/papers.jsx
+++ part2/src/components/subcomponents/papers/papers.jsx
@@ -12,5 +12,25 @@ export default class Papers extends React.Component {
     let url = config.BASEPATH + "papers"
 
     if(this.props.random !== undefined) url += "?getrandom"
+    else if (this.props.paperid !== undefined) url += `?id=${this.props.paperid}`
+    else if (this.props.award !== undefined) url += `?award=${this.props.award}`
+    else if (this.props.authorid !== undefined) url += `?authorid=${this.props.authorid}`
+
+    try {
+      let res = await fetch(url)
+      let data = await res.json()
+      if(data.status !== 200) throw new Error ((config.DEV_MODE)? `API Error: ${data.status} | ${data.message} | ${url}` : `API Error`) 
+      this.setState({results: data.results})
+    }catch(e){
+      console.log("Something went wrong ", e)
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
   }
 }

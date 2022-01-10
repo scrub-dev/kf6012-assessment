bb8204ef96ae5b117b1fe03fc90357534c5bf79b
Scrub <scott-donaldson@outlook.com>
2022-01-10 09:00:50 +0000

    Add Reading List Button given state

--- part2/src/components/subcomponents/readinglist/addreadinglistbutton.jsx
+++ part2/src/components/subcomponents/readinglist/addreadinglistbutton.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { Button } from 'react-materialize'
 
 export default class AddReadingListButton extends React.Component {
-
+  constructor(props){
+    super(props)
+    this.state = {
+      disabled: false
+    }
+  }
   addToReadingList = async () => {
     const id = this.props.paperid
   }
 
   render(){
-    return(<Button node="button">Add to Reading List</Button>)
+    let text = "Add to Reading List"
+    if(this.state.disabled) text = "Added to Reading List"
+    
+    return(<Button node="button" disabled={this.state.disabled}>{text}</Button>)
   }
 }
